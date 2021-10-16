ba4e6e996d8c30447e82f65fb81b12c20d2da604
raja-chaudhary <raja@rajachaudhary.com>
2021-10-16 11:14:10 -0700

    Learning Vuetify Components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <v-btn class="pink white--text mx-lg-3">click me</v-btn>
+    <v-btn depressed color="pink mx-lg-3">click me</v-btn>
+    <v-btn text color="pink mx-lg-3">click me</v-btn>
+
+    <v-btn depressed class="pink white--text mx-lg-3">
+      <v-icon left>mdi-email-alert</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn fab depressed dark color="purple lighter-2">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
